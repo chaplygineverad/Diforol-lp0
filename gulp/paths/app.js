01fff22795af9b0348cff6fa4084ea9eeff2749e
Kel-Kent <a.goncharenko@everad.com>
2019-01-02 13:08:51 +0200

    little fix

--- gulp/paths/app.js
+++ gulp/paths/app.js
@@ -2,8 +2,8 @@
 
 module.exports = [
     './source/js/app.js',
-  './source/js/everad.js',
-  './source/js/postDate.js',
+    './source/js/everad.js',
+    './source/js/postDate.js',
     './source/js/slick.min.js',
     './source/js/jquery-3.3.1.min.js'
 ];
